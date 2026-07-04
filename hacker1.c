0af08c35a8b8992e3995857a582085f16fc8c692
ZainabZain11 <zainabzainnn11@gmail.com>
2026-07-04 14:52:52 +0300

    Create hacker1.c

---
+++ hacker1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+    char ch;
+    char s[100];
+char sen[100];
+scanf("%c",&ch);
+getchar();
+scanf("%s",s);
+getchar();
+scanf("%[^\n]",sen);
+
+printf("%c\n",ch);
+printf("%s\n",s);
+printf("%s\n",sen);
+
+
+
+
+
+
+
+    return 0;
+}
+  
